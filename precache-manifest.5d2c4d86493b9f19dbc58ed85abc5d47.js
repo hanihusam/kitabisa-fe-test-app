d1bc947a72264cf0989acfa64d5cd1ee230d54ee
Hani Husamuddin <hani.husam@gmail.com>
2019-11-27 20:49:22 +0700

    Updates

---
+++ precache-manifest.5d2c4d86493b9f19dbc58ed85abc5d47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468ef2c0690c70ed37edde6c8a925c88",
+    "url": "/kitabisa-fe-test/index.html"
+  },
+  {
+    "revision": "0881e1675baf939e54eb",
+    "url": "/kitabisa-fe-test/static/css/main.68679d32.chunk.css"
+  },
+  {
+    "revision": "a3551bffc0da5bcf3f6d",
+    "url": "/kitabisa-fe-test/static/js/2.a2afab16.chunk.js"
+  },
+  {
+    "revision": "0881e1675baf939e54eb",
+    "url": "/kitabisa-fe-test/static/js/main.eb9954be.chunk.js"
+  },
+  {
+    "revision": "9061e71c91c6ec77f0bf",
+    "url": "/kitabisa-fe-test/static/js/runtime-main.8077cc20.js"
+  }
+]);
